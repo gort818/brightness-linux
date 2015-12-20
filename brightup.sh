ed3720d08b85b00b99030ef120130d82a00eff36
Alessandro <gort818@gmail.com>
2015-12-20 12:15:38 -0800

    Update brightup.sh

--- brightup.sh
+++ brightup.sh
@@ -12,9 +12,9 @@ newbright=$(echo "scale=2;$brightness+0.075" | bc)
 
 #increase the brightness
 
-sed -i '/ness/s/RedBrightness='$brightness'/RedBrightness='$newbright'/g' .nvidia-settings-rc
-sed -i '/ness/s/GreenBrightness='$brightness'/GreenBrightness='$newbright'/g' .nvidia-settings-rc
-sed -i '/ness/s/BlueBrightness='$brightness'/BlueBrightness='$newbright'/g' .nvidia-settings-rc
+sed -i '/ness/s/RedBrightness='$brightness'/RedBrightness='$newbright'/g' ~/.nvidia-settings-rc
+sed -i '/ness/s/GreenBrightness='$brightness'/GreenBrightness='$newbright'/g' ~/.nvidia-settings-rc
+sed -i '/ness/s/BlueBrightness='$brightness'/BlueBrightness='$newbright'/g' ~/.nvidia-settings-rc
 
 #load the configuration
 nvidia-settings --load-config-only
